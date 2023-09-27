548bc62536cfdedca5f89155b43df10a9f0dc019
Miguel <miguel@Miguels-MacBook-Pro.local>
2023-09-27 12:46:59 +0100

    Install terraform binaries

--- execute-terraform.sh
+++ execute-terraform.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-setup() {
-
-    sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-
-    wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-    gpg --no-default-keyring \
-    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-    --fingerprint
-
-    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-    sudo apt update
-
-    sudo apt-get install terraform
-
-    terraform -help
-
-}
-
 execute() {
 
     case $1 in
@@ -79,11 +55,6 @@ main() {
 
     source envs/$ENV/set-variables.sh
     cd layers/$SERVICE
-
-    if [ "$TERRAFORM_INSTALL" = true & "$ACTION" = "setup" ] ; then
-       setup
-       exit -1
-    fi
 
     execute $ACTION $SERVICE $ENV
 
