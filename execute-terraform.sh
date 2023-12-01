2f57bb1b9dc4ba2792cdc7b9c01cd7d387454112
Miguel <miguel@Miguels-MacBook-Pro.local>
2023-12-01 11:58:15 +0000

    Github secrets

--- execute-terraform.sh
+++ execute-terraform.sh
@@ -48,7 +48,7 @@ main() {
         esac
     done
 
-    export ARM_CLIENT_ID= ${{ secrets.ARM_CLIENT_ID }}
+    #export ARM_CLIENT_ID= ${{ secrets.ARM_CLIENT_ID }}
     export ARM_CLIENT_SECRET= ${{ secrets.ARM_CLIENT_SECRET }}
     export ARM_TENANT_ID= ${{ secrets.ARM_TENANT_ID }}
     export ARM_SUBSCRIPTION_ID= ${{ secrets.ARM_SUBSCRIPTION_ID }}
