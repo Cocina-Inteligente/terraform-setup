703363a862032f3911e219810c478a18d97cfce8
Miguel <miguel@Miguels-MacBook-Pro.local>
2023-12-01 13:40:46 +0000

    Github secrets

--- execute-terraform.sh
+++ execute-terraform.sh
@@ -6,12 +6,12 @@ execute() {
         init)
             printf "\n\nAbout to: $1 Terraform in $3 environment\n"
             command="terraform init -backend-config=../../envs/$3/$2/backend.tfbackend"
-            command+=" -var-file=../../envs/$3/$2/variables.tfvars -var env=$3 tenant_id=$ARM_TENANT_ID subscription_id=$ARM_SUBSCRIPTION_ID"
+            command+=" -var-file=../../envs/$3/$2/variables.tfvars -var env=$3 -var tenant_id=$ARM_TENANT_ID -var subscription_id=$ARM_SUBSCRIPTION_ID"
         ;;
         plan)   
             printf "\n\nAbout to: $1 $2 in $3 environment\n"
             command="terraform plan -no-color -out=tf.plan"
-            command+=" -var-file=../../envs/$3/$2/variables.tfvars -var env=$3 tenant_id=$ARM_TENANT_ID subscription_id=$ARM_SUBSCRIPTION_ID"
+            command+=" -var-file=../../envs/$3/$2/variables.tfvars -var env=$3 -var tenant_id=$ARM_TENANT_ID -var subscription_id=$ARM_SUBSCRIPTION_ID"
         ;;
         apply)
             printf "\n\nAbout to: $1 $2 in $3 environment\n"
